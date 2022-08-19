ca220cc981908a9b1a1d0b0890186041499638b2
zarepp <zharifothman92@gmail.com>
2022-08-20 03:32:58 +0800

    add redux state listener from ui component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,29 @@
 import styles from "../styles/Home.module.css";
 import Switch from "@mui/material/Switch";
+import { useSelector, useDispatch } from 'react-redux'
+
+// import {
+//   setRetrievePlaces,
+//   setRetrievePlacesSuccess,
+//   setRetrievePlacesFailed
+// } from "../redux/actions";
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
 export default function Home() {
- return (
-   <div className={styles.container}>
-     <div>
-       <span>With default Theme:</span>
-     </div>
-     <Switch {...label} defaultChecked />
-     <Switch {...label} />
-     <Switch {...label} disabled defaultChecked />
-   </div>
- );
+  const dispatch = useDispatch();
+
+  const places = useSelector(state => state.places.list)
+  console.log("places:", places)
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <span>With default Theme:</span>
+      </div>
+      <Switch {...label} defaultChecked />
+      <Switch {...label} />
+      <Switch {...label} disabled defaultChecked />
+    </div>
+  );
 }
