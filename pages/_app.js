5dbc3e746d1ee4055660a7bd16827b76108189e4
zarepp <zharifothman92@gmail.com>
2022-08-20 03:31:15 +0800

    add store provider and wrapper in root file

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
-import "../styles/globals.css";
 import { ThemeProvider } from "@mui/material";
+import { Provider } from "react-redux";
+
+import "../styles/globals.css";
 import { theme } from "../utils/theme";
 import createEmotionCache from "../utils/createEmotionCache";
 import { CacheProvider } from "@emotion/react";
+
+import { wrapper, store } from "../redux/store";
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -12,12 +16,14 @@ function MyApp({
  pageProps,
 }) {
  return (
-   <CacheProvider value={emotionCache}>
-     <ThemeProvider theme={theme}>
-       <Component {...pageProps} />
-     </ThemeProvider>
-   </CacheProvider>
+  <Provider store={store}>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+   </Provider>
  );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
