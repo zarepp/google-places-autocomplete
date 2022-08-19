7b3e2f90599eca1dc98715040d0eabd08141cdbc
zarepp <zharifothman92@gmail.com>
2022-08-20 06:24:51 +0800

    update search google place autocomplete mui component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
-import styles from "../styles/Home.module.css";
-import Switch from "@mui/material/Switch";
+import { useEffect, useRef, useState } from 'react';
+import s from "../styles/Home.module.css";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 import { useSelector, useDispatch } from 'react-redux'
+import {Loader} from '@googlemaps/js-api-loader';
+import usePlacesAutocomplete from "use-places-autocomplete";
 
-// import {
-//   setRetrievePlaces,
-//   setRetrievePlacesSuccess,
-//   setRetrievePlacesFailed
-// } from "../redux/actions";
+
+import {
+  setListPlaces,
+  setSelectedPlaces,
+  resetPlaces,
+} from "../redux/actions/places";
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
@@ -14,16 +19,60 @@ export default function Home() {
   const dispatch = useDispatch();
 
   const places = useSelector(state => state.places.list)
-  console.log("places:", places)
+
+  const [data, setData] = useState([])
+  const [selectedPlace, setSelectedPlace] = useState({})
+
+  const {
+    ready,
+    value,
+    suggestions: { status, data: responseData  },
+    setValue,
+  } = usePlacesAutocomplete();
+
+  useEffect(() => {
+    if (responseData.length > 0) {
+      setData(responseData)
+      dispatch(setListPlaces(responseData))
+    }
+  }, [responseData])
+
+  
+  const handleRenderInput = (params) => {
+  return (
+    <TextField
+      {...params}
+      onChange={(e) => {
+        setValue(e.target.value)
+      }}
+      onlabel="Movie"
+      margin="dense"
+    />
+  )}
 
   return (
-    <div className={styles.container}>
-      <div>
-        <span>With default Theme:</span>
+    <div className={s.container}>
+      <div className={s.margin}>
+        <span>Search Google Place:</span>
       </div>
-      <Switch {...label} defaultChecked />
-      <Switch {...label} />
-      <Switch {...label} disabled defaultChecked />
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={data || []}
+        selectOnFocus
+        sx={{ width: 600 }}
+        getOptionLabel={(option) => option.description || ""}
+        renderInput={handleRenderInput}
+        onChange={(event, value) => {
+          setSelectedPlace(value)
+        }}
+      />
+      <div className={s.margin}>
+        <span>List of Search:</span>
+      </div>
+      <div className={s.margin}>
+        <span>Select Place:</span>
+      </div>
     </div>
   );
 }
