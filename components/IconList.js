1a207943509e9387c147370814765abdd4599667
zarepp <zharifothman92@gmail.com>
2022-08-21 12:04:11 +0800

    add icon list as reusable component

---
+++ components/IconList.js
@@ -0,0 +1,26 @@
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import SearchIcon from '@mui/icons-material/Search';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+
+export default function IconList({ title }) {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360 }}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <SearchIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={title} sx={{ color: 'black' }} />
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+      </ListItem>
+    </List>
+  );
+}
